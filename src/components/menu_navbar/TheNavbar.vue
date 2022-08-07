ce30510739e03de5dbdfcd61a6807b5c4ebec051
Daniel Damasio <damasio.dan7@gmail.com>
2022-08-06 21:42:28 -0300

    created: navbar component

---
+++ src/components/menu_navbar/TheNavbar.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+// export default {}
+</script>
+
+<template>
+  <header class="navmenu">
+    <div class="navmenu__wrapper container">
+
+      <div class="navmenu__brand">
+        <div class="logo">
+          <img
+            alt="Vue logo"
+            src="@/assets/logo.svg"
+          />
+        </div>
+        <h2>TheStudents</h2>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+  .navmenu {
+    position: fixed;
+    width: 100%;
+    padding: .5rem 0;
+    background: #FFF;
+    border-bottom: 1px solid #15d160;
+    box-shadow: 0 0 20px rgba(10, 10, 10, 0.1);
+  }
+  .navmenu__brand {
+    display: flex;
+    align-items: center;
+    gap: .5rem;
+  }
+  .navmenu__brand .logo {
+    max-width: 40px;
+    flex: 1;
+  }
+  .navmenu__brand h2 {
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 600;
+  }
+</style>
